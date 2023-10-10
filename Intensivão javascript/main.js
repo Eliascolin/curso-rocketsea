6425f739ef96a590a4b0062e7f13dde6b1670427
Elias Colina <25elias.c@gmail.com>
2023-10-10 20:42:33 -0300

    finalizado intensivo javaScript

---
+++ Intensivão javascript/main.js
@@ -0,0 +1,126 @@
+/*
+
+
+
+1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, 
+exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    
+    💡 Para saber o tipo de dado você pode usar o operador `typeof`
+    
+4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for,
+ exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, 
+exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, 
+exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for,
+ exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+
+
+*/
+
+
+//1
+
+//alert('Hello World!')
+
+//2
+
+// let numberOne = Number(prompt("digite Um numero"))
+
+
+// let numberTwo = Number(prompt('Digite outro numero'))
+
+// let sumNumbers = numberOne + numberTwo
+
+
+// let result = alert(`O resultado da suma dos numeros é ${sumNumbers} `)
+
+
+//3 e 4
+
+
+// let dado = Number(prompt('digite um numero'))
+
+// if(dado){
+
+// alert(`é um numero`)
+
+// }else(alert(`é uma string`))
+
+
+
+// 5
+
+
+
+// let myVar = true; 
+
+
+// if (typeof myVar === 'boolean') {
+//   alert("É um booleano");
+// } else {
+//   alet("Não é um booleano");
+// }
+
+
+
+//6
+
+
+//  let numberOne = Number(prompt("digite Um numero"))
+
+
+//  let numberTwo = Number(prompt('Digite outro numero'))
+
+//  let restNumbers = numberOne - numberTwo
+
+
+// let result = alert(`O resultado da subtraçao dos numeros é ${restNumbers} `)
+
+// 7
+
+
+// let numberOne = Number(prompt("digite Um numero"))
+
+
+//  let numberTwo = Number(prompt('Digite outro numero'))
+
+//  let multNumbers = numberOne * numberTwo
+
+
+// let result = alert(`O resultado da multiplicaçao  dos numeros é ${multNumbers} `)
+
+
+//8
+
+// let numberOne = Number(prompt("digite Um numero"))
+
+
+//  let numberTwo = Number(prompt('Digite outro numero'))
+
+// let divNumbers = numberOne / numberTwo
+
+
+//  let result = alert(`O resultado da diviçao  dos numeros é ${divNumbers} `)
+
+
+// 9 - 10
+
+
+// let number = Number(prompt("digite Um numero"))
+
+
+
+// let result = number % 2 === 0 ?  "é par" :  "impar"
+
+// alert(result)
+
+
+//10
