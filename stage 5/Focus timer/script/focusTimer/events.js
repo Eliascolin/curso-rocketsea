d18abaf1077b95f2e3e9627aa271248ccce36711
elias <25elias.c@gmail.com>
2023-12-09 20:46:32 -0300

    Registrando eventos dos botões

--- stage 5/Focus timer/script/focusTimer/events.js
+++ stage 5/Focus timer/script/focusTimer/events.js
@@ -4,6 +4,19 @@ import { controls } from "./elements.js";
 export function registerControls(){
 
 
-controls.addEventListener('click',(event)=>{console.log(event.target)})
+controls.addEventListener('click',(event)=>{
+    
+    
+   const action = event.target.dataset.action
 
+if(action === undefined){
+
+    return
 }
+
+console.log(action)
+}
+
+
+
+)}
